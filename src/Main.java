5d770196cda4578457b390d75ad802d57bb92f4d
Roman <romka.polupan@gmail.com>
2026-01-06 17:41:52 +0300

    Небольшое изменение

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //Задача 1
+        //Задача N1
         int age = 28;
         if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + " он совершеннолетний");
@@ -10,21 +10,21 @@ public class Main {
         if (age <= 18) {
             System.out.println("он не достиг совершеннолетия, нужно немного подождать");
         }
-        //Задача 2
+        //Задача N2
         int t = -3;
         if (t >= 5) {
             System.out.println("На улице " + t + " градусов, можно идти без шапки");
         } else {
             System.out.println("На улице " + t + " градусов, нужно надеть шапку");
         }
-        //Задача 3
+        //Задача N3
         int kmH = 55;
         if (kmH >= 60) {
             System.out.println("Если скорость " + kmH + ", то придется заплатить штраф");
         } else {
             System.out.println("Если скорость " + kmH + ", то можно ездить спокойно");
         }
-        //Задача 4
+        //Задача N4
         int person = 22;
         if (person >= 2 && person <= 6) {
             System.out.println("Если возраст человека равен " + person + ", то ему нужно ходить в детский сад");
@@ -38,7 +38,7 @@ public class Main {
         if (person > 24) {
             System.out.println("Если возраст человека равен " + person + ", то ему пора на работу");
         }
-        //Задача 5
+        //Задача N5
         int child = 7;
         if (child <= 5) {
             System.out.println("Если возраст ребенка равен " + child + ": нельзя кататься на атракционе");
@@ -49,14 +49,14 @@ public class Main {
         if (child > 15) {
             System.out.println("Если возраст ребенка равен " + child + ": можно кататься в сопровождении взрослого");
         }
-        //Задача 6
+        //Задача N6
         int people = 102;
         if (people >= 60) {
             System.out.println("Сидячих мест нету - вагон полностью забит");
         } else {
             System.out.println("В вагоне есть сидячие места");
         }
-        //Задача 7
+        //Задача N7
         int one = 1;
         int two = 2;
         int three = 3;
